35a1a850f582e53d892f4f4dae2b5748156e113e
John Daskovsky <johndaskovsky@gmail.com>
2017-01-21 22:23:49 -0500

    put group select before recipe button

--- public_html/wp-content/plugins/mokamrp/actions/mokamrp_home.php
+++ public_html/wp-content/plugins/mokamrp/actions/mokamrp_home.php
@@ -32,11 +32,11 @@ if ( current_user_can('manage_options') ) {
   $groups = get_all_table_rows("groups");
   foreach ($recipes as $row) {
     echo "<form action=\"admin.php?page=mokamrp_new_actions&amp;recipe_id={$row['id']}\" method=\"post\">";
-    echo "<div class=\"form-actions\">
-      <input type=\"submit\" name=\"submit\" id=\"submit\" class=\"btn btn-primary\" value=\"{$row['name']}\">&nbsp;&nbsp;&nbsp;";
+    echo "<div class=\"form-actions\">";
     echo "<select id=\"group_id\" style=\"margin-bottom:0;\" class=\"input-large\" name=\"group_id\" required>";
     echo array_to_option_list($groups, "id", "name", NULL, NULL, TRUE, ""); 
     echo "</select>";
+    echo "&nbsp;&nbsp;&nbsp;<input type=\"submit\" name=\"submit\" id=\"submit\" class=\"btn btn-primary\" value=\"{$row['name']}\">";
     echo "</div>";
     echo "</form>";
   }
