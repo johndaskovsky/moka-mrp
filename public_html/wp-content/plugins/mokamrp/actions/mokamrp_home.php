8e32613d7d93b778150af1b552badb200c9c6c77
John Daskovsky <johndaskovsky@gmail.com>
2017-01-21 22:41:35 -0500

    use the recipe groups selection to limit the possible selection on the actions page

--- public_html/wp-content/plugins/mokamrp/actions/mokamrp_home.php
+++ public_html/wp-content/plugins/mokamrp/actions/mokamrp_home.php
@@ -29,12 +29,28 @@ if ( current_user_can('manage_options') ) {
 
   //Other recipes
   $recipes = get_all_table_rows("recipes");
-  $groups = get_all_table_rows("groups");
+  $all_groups = get_all_table_rows("groups");
   foreach ($recipes as $row) {
     echo "<form action=\"admin.php?page=mokamrp_new_actions&amp;recipe_id={$row['id']}\" method=\"post\">";
     echo "<div class=\"form-actions\">";
     echo "<select id=\"group_id\" style=\"margin-bottom:0;\" class=\"input-large\" name=\"group_id\" required>";
+    
+    $groups_string = get_recipe_groups($row['id']);
+    if($groups_string == "") {
+      $groups = $all_groups;
+    } else {
+      $groups_array = explode(",", $groups_string);
+      $groups = [];
+      foreach($all_groups as $group_row)
+      {
+        if(in_array($group_row['id'], $groups_array)){
+          array_push($groups,$group_row);
+        }
+      }
+    }
+
     echo array_to_option_list($groups, "id", "name", NULL, NULL, TRUE, ""); 
+
     echo "</select>";
     echo "&nbsp;&nbsp;&nbsp;<input type=\"submit\" name=\"submit\" id=\"submit\" class=\"btn btn-primary\" value=\"{$row['name']}\">";
     echo "</div>";
